b1d25e132d95ee8430f2ecc1b830b0afef617b71
Shanur <shanurislam@gmail.com>
2024-02-16 16:36:50 +0000

    updated jsparticle and main body to be flex

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-
-    new JParticles.Particle('#demo', {
-        color: '#505050',
-        lineShape: 'spider',
-        range: 2000,
-        proximity: 100,
-        parallax: false,
-    })
     
     var contentElements = document.querySelectorAll('.navContent');
     var mainContentElements = document.querySelectorAll('.mainContent');
@@ -37,14 +29,20 @@ document.addEventListener("DOMContentLoaded", function () {
     
     var lightThemeElement = document.querySelector('.lightTheme');
     var darkThemeElement = document.querySelector('.darkTheme');
+    var gitHubElement = document.querySelector('.gitHub');
 
     lightThemeElement.addEventListener('click', function () {
-        document.documentElement.style.setProperty('--d-bg-color', '#f5f5f5');
+        document.documentElement.style.setProperty('--d-bg-color', '#dcd4c7');
         document.documentElement.style.setProperty('--d-t-color', '#0d0d0d');
     });
 
     darkThemeElement.addEventListener('click', function () {
         document.documentElement.style.setProperty('--d-bg-color', '#0d0d0d');
         document.documentElement.style.setProperty('--d-t-color', '#f5f5f5');
+    });
+
+    gitHubElement.addEventListener('click', function (event) {
+        event.preventDefault();
+        window.open(`https://github.com/sunrah27`, `_blank`);
     });
 });
