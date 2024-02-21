4862def1115269ffd86425698da2882ce8744d31
Shanur <shanurislam@gmail.com>
2024-02-21 14:47:51 +0000

    Updated media queries and mobile menu
    - Added mobile menu
    - Added menu animations
    - Added Burger menu icon
    - Used CSS to change the icons colour
    - Added various media queries

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
     
+    // Display mobile menu
+    const menuItems = document.getElementById('nav-menu');
+    const menuIcon = document.getElementById('menuIcon');
+    const myName = document.getElementById('myName');
+    const role = document.getElementById('role');
+
+    menuItems.style.maxHeight = '0px';
+
+    menuIcon.addEventListener('click', () => {
+        if (menuItems.style.maxHeight === '0px') {
+            myName.style.maxHeight = '0px';
+            role.style.maxHeight = '0px';
+            setTimeout(() => {
+                menuItems.style.maxHeight = '200px';
+            }, 500);
+        } else {
+            menuItems.style.maxHeight = '0px';
+            setTimeout(() => {
+                if(menuItems.style.maxHeight = '0px') {
+                    myName.style.maxHeight = '200px';
+                    role.style.maxHeight = '200px';
+                }
+            }, 500);
+        }
+    });
+
     var contentElements = document.querySelectorAll('.navContent');
     var mainContentElements = document.querySelectorAll('.mainContent');
 
@@ -27,11 +53,13 @@ document.addEventListener("DOMContentLoaded", function () {
     lightThemeElement.addEventListener('click', function () {
         document.documentElement.style.setProperty('--d-bg-color', '#dcd4c7');
         document.documentElement.style.setProperty('--d-t-color', '#0d0d0d');
+        menuIcon.style.setProperty('filter', 'brightness(50%) contrast(200%)');
     });
 
     darkThemeElement.addEventListener('click', function () {
         document.documentElement.style.setProperty('--d-bg-color', '#0d0d0d');
         document.documentElement.style.setProperty('--d-t-color', '#f5f5f5');
+        menuIcon.style.setProperty('filter', 'invert(100%)');
     });
 
     gitHubElement.addEventListener('click', function (event) {
